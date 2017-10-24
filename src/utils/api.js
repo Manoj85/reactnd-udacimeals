55db1e864f6d3724b31b4012d7193987c414ec6d
Manoj Talagadadeevi <manojt.854@gmail.com>
2017-10-24 14:27:11 -0400

    Logger

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,12 @@
-const API_ID = process.env.REACT_APP_API_ID
-const APP_KEY = process.env.REACT_APP_APP_KEY
+// const API_ID = process.env.REACT_APP_API_ID
+// const APP_KEY = process.env.REACT_APP_APP_KEY
+
+const API_ID = '1d467add'
+const APP_KEY = 'ce4d48a6353c8c08574cdccf0847b630'
+
+console.log(process.env)
+console.log(API_ID)
+console.log(APP_KEY)
 
 export function fetchRecipes (food = '') {
     food = food.trim()
